ea9815e7c73e71725f4a62766ad8b870ba8b1bc8
Paul V Nguyen <paulngyn512@gmail.com>
2024-11-25 00:12:08 -0800

    Added logo image and banner text

--- src/app/components/HomeBanner/HomeBanner.tsx
+++ src/app/components/HomeBanner/HomeBanner.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
-import styles from './HomeBanner.module.css'; // Assuming you have specific styles for the logo
+import styles from './HomeBanner.module.css';
 import bannerImage from '../../public/ezhr_banner.jpg';
 
 const HomeBanner: React.FC = () => {
   return (
     <div className={styles.homebanner}>
-          <Image src={bannerImage} alt="HomeBanner" layout="fill" objectFit="cover" quality={100} />
+      <Image src={bannerImage} alt="HomeBanner" layout="fill" objectFit="cover" quality={100} />
+      <div className={styles.bannerText}>Onboard Smarter not Harder</div>
     </div>
   );
 };
