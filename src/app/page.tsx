bc47cdde9db5bb09ddc02beeadacd47adce0de5d
Paul V Nguyen <paulngyn512@gmail.com>
2024-12-01 20:32:51 -0800

    Finished home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from 'react';
 import NavBar from '../app/components/NavBar/NavBar';
 import HomeBanner from './components/HomeBanner/HomeBanner';
+import FindOutMore from './components/FindOutMore/FindOutMore';
+import HomeTextBlock from './components/HomeTextBlock/HomeTextBlock';
 
 
 export default function Home() {
@@ -12,6 +14,8 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <NavBar setPage={setPage} />
       <HomeBanner/>
+      <HomeTextBlock/>
+      <FindOutMore setPage={setPage} />
     </div>
   );
 }
